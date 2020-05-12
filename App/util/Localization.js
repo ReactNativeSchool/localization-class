18d65c852aa1c0854c1bb7271dade78fc9405ee1
Spencer Carli <spencercarli@gmail.com>
2020-05-12 10:09:52 -0500

    05 Using System Language

--- App/util/Localization.js
+++ App/util/Localization.js
@@ -1,10 +1,16 @@
 import i18n from 'i18n-js';
+import * as RNLocalize from 'react-native-localize';
 
 export const initialize = () => {
-  i18n.locale = 'en';
+  const locale = RNLocalize.getLocales()[0].languageCode;
+
+  i18n.locale = locale;
   i18n.translations = {
     en: {
       currencyConverter: 'Currency Converter',
+    },
+    es: {
+      currencyConverter: 'Convertidor de Moneda',
     },
   };
 };

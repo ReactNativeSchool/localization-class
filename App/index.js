7b4f2651ce1f83c08ef0dbfc35ab66a45457ddd4
moshe vilner <shootermv@gmail.com>
2021-06-11 18:02:55 +0300

    feat:add a bootsplash

--- App/index.js
+++ App/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import RNBootSplash from "react-native-bootsplash";
 import Navigation from './config/Navigation';
 import { ConversionContextProvider } from './util/ConversionContext';
 import { useLocalization } from './util/Localization';
 
+
 export default () => {
+  RNBootSplash.hide(); // immediate
   const { localizationConfigured } = useLocalization();
 
   if (!localizationConfigured) {

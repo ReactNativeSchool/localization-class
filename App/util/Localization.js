497edef7feac6bfce4be2184fdf0993e66ea8f33
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-03-08 13:14:03 -0600

    Detecting System Language Changes

--- App/util/Localization.js
+++ App/util/Localization.js
@@ -1,16 +1,30 @@
+import { useEffect } from 'react';
 import i18n from 'i18n-js';
 import * as RNLocalize from 'react-native-localize';
 
-export const initialize = () => {
-  const locale = RNLocalize.getLocales()[0].languageCode;
+export const useLocalization = () => {
+  useEffect(() => {
+    i18n.translations = {
+      en: {
+        currencyConverter: 'Currency Converter',
+      },
+      es: {
+        currencyConverter: 'Convertidor de Moneda',
+      },
+    };
 
-  i18n.locale = locale;
-  i18n.translations = {
-    en: {
-      currencyConverter: 'Currency Converter',
-    },
-    es: {
-      currencyConverter: 'Convertidor de Moneda',
-    },
-  };
+    const handleLocalizationChange = () => {
+      const locale = RNLocalize.getLocales()[0].languageCode;
+
+      i18n.locale = locale;
+    };
+
+    handleLocalizationChange();
+
+    RNLocalize.addEventListener('change', handleLocalizationChange);
+
+    return () => {
+      RNLocalize.removeEventListener('change', handleLocalizationChange);
+    };
+  }, []);
 };
